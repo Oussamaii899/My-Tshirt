dcf67d4890d3a10b8c340cbc5e47b0bfd6a80aac
Oussamaii899 <oussama.sbaa47@gmail.com>
2024-12-09 16:26:21 +0100

    edit navbar and css of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "/css/Nav.css";
 
 export default function Nav() {
     return(
@@ -8,9 +7,10 @@ export default function Nav() {
             <nav>
                 <div className="logo"><span style={{color:"lightblue"}}>t-</span>shurt</div>
                     <ul>
-                        <li><input type="text" placeholder="Cherchez un short" name="" id="" /><button type="submit">Search</button></li>
-                        <li><div className="lien">compte</div></li>
-                        <li><div className="lien">Panier</div></li>
+                        <li id="searchli"><input id="search-bar" type="text" placeholder="Cherchez un short" name="" /><button type="submit"><i className="fa fa-search"></i></button></li>
+                        <li><div className="lien"> <i className="fa fa-user"></i></div></li>
+                        <li><div className="lien"><i className="fa fa-shopping-cart"></i>
+                        </div></li>
                     </ul>
             </nav>
         </header>
